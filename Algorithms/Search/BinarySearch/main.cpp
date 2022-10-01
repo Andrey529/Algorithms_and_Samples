389542f65f482e1ee64481f8c9c7ae567a504816
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-10-02 01:29:27 +0300

    improved binary search implementation

--- Algorithms/Search/BinarySearch/main.cpp
+++ Algorithms/Search/BinarySearch/main.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
 #include <vector>
 
-template<typename T>
-int binarySearch(const std::vector<T>& arr, const T& item) {
-    int low = 0;
-    int high = arr.size() - 1;
+template<typename Collection>
+using ValType = typename Collection::value_type;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        T guess = arr[mid];
-        if (guess == item) {
-            return mid;
+template<typename Collection>
+using SizeType = typename Collection::size_type;
+
+// O(logN)
+template<typename Collection>
+int binarySearch(const Collection& collection, const ValType<Collection>& value) {
+    SizeType<Collection> begin = 0u;
+    SizeType<Collection> end = collection.size() - 1;
+    SizeType<Collection> mid = end / 2;
+
+    while (begin <= end) {
+        ValType<Collection> guess = collection[mid];
+        if (guess < value) {
+            begin = mid + 1;
+            mid = (begin + end) / 2;
         }
-        else if (guess > item) {
-            high = mid - 1;
+        else if (guess > value) {
+            end = mid - 1;
+            mid = (begin + end) / 2;
         }
         else {
-            low = mid + 1;
+            return static_cast<int>(mid);
         }
     }
     return -1;
 }
 
 int main(){
-    std::vector<int> arr{1, 2, 3, 4, 6, 7, 8};
+    std::vector<int> arr = {1, 2, 3, 4, 6, 7, 8};
 
     std::cout << binarySearch(arr, 6) << std::endl;
     std::cout << binarySearch(arr, 5) << std::endl;
