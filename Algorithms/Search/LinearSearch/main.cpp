9aa4dd24c44ce5d7b7acffc5c2820550c7527149
Andrey <75383288+Andrey529@users.noreply.github.com>
2023-06-02 16:48:02 +0300

    added linear search

---
+++ Algorithms/Search/LinearSearch/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+template<typename Collection>
+using ValType = typename Collection::value_type;
+
+template<typename Collection>
+using SizeType = typename Collection::size_type;
+
+// O(N)
+template<typename Collection>
+int binarySearch(const Collection& collection, const ValType<Collection>& value) noexcept {
+    for (SizeType<Collection> i = 0; i < collection.size(); ++i) {
+        if (collection[i] == value) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main(){
+    std::vector<int> arr = {1, 2, 3, 4, 6, 7, 8};
+
+    std::cout << binarySearch(arr, 6) << std::endl;
+    std::cout << binarySearch(arr, 5) << std::endl;
+
+    return 0;
+}
